139f6ac5f0286b6f85ab295f0cdb6e2b6008982e
Diogo Seidi Kitahara <119024296+DiogoKitahara@users.noreply.github.com>
2024-09-20 18:41:09 -0300

    Update AgenciaBancaria.js

--- Atividade3/AgenciaBancaria.js
+++ Atividade3/AgenciaBancaria.js
@@ -1,50 +1,51 @@
 import { Conta } from "./Conta.js";
 
 class AgenciaBancaria {
-    #listaConta
-    #codigo
+  listaConta
+  codigo
 
-    constructor() {
-        this.#codigo = 0;
-        this.#listaConta = [];
-    }
+  constructor() {
+    this.codigo = 0;
+    this.listaConta = [];
+  }
 
-    getCodigo() {
-        return this.#codigo;
-    }
-    setCodigo(codigo) {
-        this.#codigo = codigo;
-    }
+  getCodigo() {
+    return this.codigo;
+  }
+  setCodigo(codigo) {
+    this.codigo = codigo;
+  }
 
-    addConta(objConta) {
-        this.#listaConta.push(objConta);
-    }
+  addConta(objConta) {
+    console.log("Conta Adicionada com sucesso");
+    this.listaConta.push(objConta);
+  }
 
-    getConta(numero) {
-        for (let i = 0; i < this.#listaConta.length; i++) {
-            if (numero == this.#listaConta[i].numero) {
-                console.log("Conta encontrada com sucesso");
-                return this.#listaConta[i];
-            }
-        }
-        console.log("Numero de conta nao encontrada");
+  getConta(numero) {
+    for (let i = 0; i < this.listaConta.length; i++) {
+      if (numero == this.listaConta[i].numero) {
+        console.log("Conta encontrada com sucesso");
+        return this.listaConta[i];
+      }
     }
+    console.log("Numero de conta nao encontrada");
+  }
 
-    listContas() {
-        let str = '';
-        let contador = 1;
-        for (let i = 0; i < this.#listaConta.length; i++) {
-            let numero = this.#listaConta[i].numero;
-            let titular = this.#listaConta[i].titular;
-            let saldo = this.#listaConta[i].saldo;
-            str = str + '\n\nConta ' + contador +
-            '\nNumero: ' + numero + 
-            '\nTitular: ' + titular + 
-            '\nSaldo: ' + saldo;
-            contador++;
-        }
-        return str;
+  listContas() {
+    let str = '';
+    let contador = 1;
+    for (let i = 0; i < this.listaConta.length; i++) {
+      let numero = this.listaConta[i].numero;
+      let titular = this.listaConta[i].titular;
+      let saldo = this.listaConta[i].saldo;
+      str = str + '\n\nConta ' + contador +
+        '\nNumero: ' + numero +
+        '\nTitular: ' + titular +
+        '\nSaldo: ' + saldo;
+      contador++;
     }
+    return str;
+  }
 }
 
-export {AgenciaBancaria};
+export { AgenciaBancaria };
